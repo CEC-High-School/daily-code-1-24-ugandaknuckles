38648aa251b8ef8ddb444fd331d53d2330c5f1b3
ugandaknuckles <35495803+ugandaknuckles@users.noreply.github.com>
2018-01-24 08:11:35 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,24 @@
+#include<iostream>
+using namespace std;
+int number;
+int main() {
+	cout << "enter a number" << endl;
+	cin >> number;
+	if (50 > number)
+		cout << "you suck" << endl;
+	else if (100 < number)
+		cout << "you are cool" << endl;
+	else 
+		cout << "nice" << endl;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+
+
+
+
+
+	system("pause");
+}
